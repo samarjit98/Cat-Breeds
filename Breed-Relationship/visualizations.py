ec403e08b36ab0107261ea09f30df3a38a967b42
Dilisha Jain <kaladilisha.jain@gmail.com>
2019-04-17 14:10:14 +0530

    visualizations

--- Breed-Relationship/visualizations.py
+++ Breed-Relationship/visualizations.py
@@ -24,3 +24,52 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('==> Loading network..')
 rowcnn = RowCNN().to(device)
 rowcnn.load_state_dict(torch.load('./weights/network.ckpt'))
+l_list = list(rowcnn.children())
+l_list = list(l_list[0].children())
+features = torch.nn.Sequential(*l_list[:-1]) 
+
+cats = [
+		'cat1',
+		'cat2'
+	]
+
+cat_breeds = [
+        'Abyssinian',
+        'Bengal',
+        'Birman',
+        'Bombay',
+        'British_Shorthair',
+        'Egyptian_Mau',
+        'Maine_Coon',
+        'Persian',
+        'Ragdoll',
+        'Russian_Blue',
+        'Siamese',
+        'Sphynx'
+    ]
+
+def visualization():
+
+	for catbreed in cat_breeds:
+		os.makedirs("./{}".format(catbreed), exist_ok=True)
+
+	for idx in range(len(cats)):			
+		input = cv2.imread(cats[idx])
+		input = input.transpose(1, 2, 0) 
+		input = input.transpose(1, 2, 0) 
+		input = input.unsqueeze(0)
+		input = torch.tensor(input).type(torch.FloatTensor)	#converting to tensor 
+		input = input.to(device)
+		images = features(input)
+		label = 1
+		for image in images:
+		   image = image[0].detach().cpu().numpy()
+		   image = image.transpose(1, 2, 0)
+           image = image.transpose(1, 2, 0)
+           cv2.imwrite("./{}/visuals_{}.png".format(cat_breeds[idx], label),image)
+           label = label + 1            
+
+
+
+if __name__=="__main__":
+	visualization()
