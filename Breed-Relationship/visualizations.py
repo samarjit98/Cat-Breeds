7927ead6e53f65ddc6504c08e610e7fb706a01fb
Dhruv <dhruvramani98@gmail.com>
2019-04-29 20:00:00 +0530

    visuals

--- Breed-Relationship/visualizations.py
+++ Breed-Relationship/visualizations.py
@@ -25,12 +25,24 @@ print('==> Loading network..')
 rowcnn = RowCNN().to(device)
 rowcnn.load_state_dict(torch.load('./weights/network.ckpt'))
 l_list = list(rowcnn.children())
-l_list = list(l_list[0].children())
-features = torch.nn.Sequential(*l_list[:-1]) 
+print(l_list)
+#features = torch.nn.Sequential(*l_list[:-1])
+features = l_list[0]
+print(features) 
 
 cats = [
-		'cat1',
-		'cat2'
+		'../../../../../dataset/Pet_cats2/Abyssinian/Abyssinian_80.jpg',
+		'../../../../../dataset/Pet_cats2/Bengal/Bengal_147.jpg',
+		'../../../../../dataset/Pet_cats2/Birman/Birman_86.jpg',
+		'../../../../../dataset/Pet_cats2/Bombay/Bombay_22.jpg',
+		'../../../../../dataset/Pet_cats2/British_Shorthair/British_Shorthair_18.jpg',
+		'../../../../../dataset/Pet_cats2/Egyptian_Mau/Egyptian_Mau_183.jpg',
+		'../../../../../dataset/Pet_cats2/Maine_Coon/Maine_Coon_164.jpg',
+		'../../../../../dataset/Pet_cats2/Persian/Persian_94.jpg',
+		'../../../../../dataset/Pet_cats2/Ragdoll/Ragdoll_10.jpg',
+		'../../../../../dataset/Pet_cats2/Russian_Blue/Russian_Blue_119.jpg',
+		'../../../../../dataset/Pet_cats2/Siamese/Siamese_146.jpg',
+		'../../../../../dataset/Pet_cats2/Sphynx/Sphynx_233.jpg'
 	]
 
 cat_breeds = [
@@ -57,17 +69,23 @@ def visualization():
 		input = cv2.imread(cats[idx])
 		input = input.transpose(1, 2, 0) 
 		input = input.transpose(1, 2, 0) 
-		input = input.unsqueeze(0)
 		input = torch.tensor(input).type(torch.FloatTensor)	#converting to tensor 
+		input = input.unsqueeze(0)
 		input = input.to(device)
-		images = features(input)
+		print(input.shape)
+		images = []
+		for conv in features:
+			images.append(conv(input))
 		label = 1
 		for image in images:
-		   image = image[0].detach().cpu().numpy()
-		   image = image.transpose(1, 2, 0)
-           image = image.transpose(1, 2, 0)
-           cv2.imwrite("./{}/visuals_{}.png".format(cat_breeds[idx], label),image)
-           label = label + 1            
+			image = image[0].detach().cpu().numpy()
+			for i in range(image.shape[0]):
+				imagecp = image[i]
+				#image = image.transpose(1, 2, 0)
+				#image = image.transpose(1, 2, 0)
+				print(imagecp.shape)
+				cv2.imwrite("./{}/visuals_{}.png".format(cat_breeds[idx], label),imagecp)
+				label = label + 1            
 
 
 
